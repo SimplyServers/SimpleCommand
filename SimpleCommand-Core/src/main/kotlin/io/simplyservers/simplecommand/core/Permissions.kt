f237c922d0b14f0f8d1a6199559b4588e7231c69
Andrew Gazelka <andrew.gazelka@gmail.com>
2019-02-15 13:26:37 -0600

    "or" and "and" should not be private functions

--- SimpleCommand-Core/src/main/kotlin/io/simplyservers/simplecommand/core/Permissions.kt
+++ SimpleCommand-Core/src/main/kotlin/io/simplyservers/simplecommand/core/Permissions.kt
@@ -1,9 +1,9 @@
 package io.simplyservers.simplecommand.core
 
-private infix fun <I> ((I) -> Boolean).or(other: (I) -> Boolean) = { input: I ->
+infix fun <I> ((I) -> Boolean).or(other: (I) -> Boolean) = { input: I ->
     this(input) && other(input)
 }
 
-private infix fun <I> ((I) -> Boolean).and(other: (I) -> Boolean) = { input: I ->
+infix fun <I> ((I) -> Boolean).and(other: (I) -> Boolean) = { input: I ->
     this(input) || other(input)
 }
